4564582e07a330000ee59536f0c820e26610d531
Natella <natellabondareva20@gmail.com>
2024-02-29 10:15:43 +0200

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть коефіцієнт:");
+            int k = int.Parse(Console.ReadLine());
+            Console.WriteLine("0 - автоматичне заповненя масиву\n1 - ввід масиву вручну");
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть кількість елементів масиву:");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            switch (choice) 
+            {
+                case 0: GenerateRandomArray(ref array);
+                    PrintArray(array);
+                    break;
+                case 1: InputArray(ref array);
+                    break;
+                default: Console.WriteLine("");
+                    break;
+            }
+            int[] array2 = OwnMethod(array, k);
+            PrintArray(array2);
+
+        }
+        static void InputArray(ref int[] array)
+        {
+            Console.WriteLine("Введіть елементи масиву через пробіл:");
+            string[] str = Console.ReadLine().Trim().Split();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(str[i]);
+            }
+        }
+        static void GenerateRandomArray(ref int[] array)
+        {
+            Random random = new Random();   
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0,10000);
+            }
+        }
+        private static void PrintArray(int[] array)
+        {
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine(); 
+        }
+        private static int[] CoolMethodWithEnumerable(int[] originalArray, int k)
+        { 
+            var newArray = originalArray.Where(element => element % k == 0).ToArray();
+            return newArray;
+        }
+        private static int[] OwnMethod(int[] originalArray, int k)
+        {
+            int temp = 0;
+            foreach (int element in originalArray)
+            {
+                if (element % k == 0)
+                {
+                    temp++;
+                }
+            }
+            int[] resultArray = new int[temp];
+            temp = 0;
+            foreach (int element in originalArray)
+            {
+                if (element % k == 0)
+                {
+                    resultArray[temp++] = element;
+                }
+            }
+            return resultArray;
+        }
+    }
+}
---
+++ lab1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Program
+    {
+        class Timer
+        {
+            public delegate void TimerDelegate();
+
+            private TimerDelegate timerDelegate;
+            private int interval;
+            private bool isRunning;
+
+            public Timer(TimerDelegate method, int intervalInSeconds)
+            {
+                timerDelegate = method;
+                interval = intervalInSeconds * 1000; // перетворюємо секунди на мілісекунди
+                isRunning = false;
+            }
+
+            public void Start()
+            {
+                if (isRunning)
+                {
+                    Console.WriteLine("Timer is already running.");
+                    return;
+                }
+
+                isRunning = true;
+
+                Thread timerThread = new Thread(() =>
+                {
+                    while (isRunning)
+                    {
+                        timerDelegate.Invoke(); // викликаємо делегат
+
+                        Thread.Sleep(interval);
+                    }
+                });
+
+                timerThread.Start();
+            }
+
+            public void Stop()
+            {
+                isRunning = false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Timer.TimerDelegate myDelegate = () =>
+            {
+                Console.WriteLine("Method executed at: " + DateTime.Now);
+            };
+
+            Timer myTimer = new Timer(myDelegate, 5); // викликати метод кожні 5 секунд
+
+            myTimer.Start();
+
+            // Дайте таймеру працювати протягом якогось часу
+            Thread.Sleep(20000);
+
+            myTimer.Stop(); // Зупиняємо таймер
+        }
+    }
+}
